85ee9230c1b0f760082fc5e54a88ddafd4091430
Luca Weiss <luca.weiss@fairphone.com>
2022-06-24 14:08:48 +0200

    Vulkan fixup

--- device-common.mk
+++ device-common.mk
@@ -62,8 +62,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
 	vulkan.freedreno
 
-# FIXME Vulkan 1.2 supported
-# FIXME deqp level 2022-03-01?
+# FIXME Driver exposes Vulkan 1.2
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
